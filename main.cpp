d21137c998cbe78a1f794bdb7928c26fbef2c450
U-BSD\323382 <323382@SHS-10D9C06DF.beaverton.k12.or.us>
2019-02-26 09:13:36 -0800

    began heap class

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+#include "Heap.h"
+
+using namespace std;
+
+int main(){
+  //find if file or command line
+
+  //define length of array as you go
+  
+  //create array from commamd line
+
+  //creare array from file
+
+  //create heap
+
+  //display tree
+
+  //output in order
+  
+  return 0;
+}
